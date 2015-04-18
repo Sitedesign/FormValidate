31fd14c2a97f8e439dd7b718f310b1fb46d0609d
Krisztian CSANYI <csanyikrisztian@users.noreply.github.com>
2015-04-18 10:00:35 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,32 @@
+<?php
+
+include("class.formvalidate.php");
+
+$rules = array(
+    "name" => array("required" => true, "minlength" => 2, "maxlength" => 50),
+    "email" => array("required" => true, "type" => "email"),
+    "url" => array("type" => "url"),
+    "comment" => array("required" => true, "minlength" => 10),
+    "comment_post_id" => array("type" => "numeric"),
+    "comment_parent" => array("type" => "numeric"),
+    "captcha" => array("minlength" => 6, "maxlength" => 6)
+);
+
+$_POST = array(
+    "name" => "Chris",
+    "email" => "aaa@bbb.cc",
+    "url" => "http://www.foo.com",
+    "comment" => "bla bla bla",
+    "comment_post_id" => "bla bla bla",     // wrong
+    "comment_parent" => 7,
+    "captcha" => "aaaaaa"
+);
+
+try {
+    $fv = new FormValidate($_POST, $rules);
+    $fv->validate();
+} catch (FormValidateException $e) {
+    echo "The ".$e->getField()." variable doesn't match the ".$e->getProperty()." property.";
+}
+
+?>
